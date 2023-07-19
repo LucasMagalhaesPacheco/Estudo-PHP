a6db96a2ebfcad48dd24957cbcf5fb3fd35a943f
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-07-18 21:49:16 -0300

    A

--- MySQL/insertPDO/index.php
+++ MySQL/insertPDO/index.php
@@ -9,7 +9,8 @@ $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $stmt = $conn->prepare("INSERT INTO itens (name, valor) VALUES (:name, :valor)");
 
 $name = "Suporte monitor";
-$valor = 15;
+$valor = 15; 
+$A =1231212;
 
 $stmt->bindParam(":name", $name);
 $stmt->bindParam(":valor", $valor);
